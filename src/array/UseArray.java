a165f303eb211e546353774f3f0773f7ae19b420
Jahidul2543 <jahidul2543@gmail.com>
2018-08-19 12:57:41 -0400

    Scanner is added

--- src/array/UseArray.java
+++ src/array/UseArray.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class UseArray {
     public static void main(String[] args) {
-        //Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         Random random = new Random();
         int [] array = new int[4000];
         System.out.println("plz enter numbers: ");
